d2efb15302ab3494a6d312c166f83cf2443b0791
ramirogrisales <ramirogrisales@gmail.com>
2023-09-30 10:35:21 -0500

    feat: Home & Header components creation

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/react'
+
+export const Header = () => {
+  return (
+    <Box bg='tomato' w='100%' h={80} p={5} color='white' display='flex' alignItems='center'>
+      Commits Notificator App
+    </Box >
+  )
+}
---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,10 @@
+import { Container } from '@chakra-ui/react'
+import { Header } from '../../components/Header/Header'
+
+export const Home = () => {
+  return (
+    <Container w='100vw' h='100vh'>
+      <Header/>
+    </Container>
+  )
+}
